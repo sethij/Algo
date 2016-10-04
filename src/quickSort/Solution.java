d17e68351dc256b99e5347f0a01b5a9ba75982f0
Jagriti Sethi <sethi.jagriti@gmail.com>
2016-10-04 17:39:26 +0530

    quick sort updated

--- src/quickSort/Solution.java
+++ src/quickSort/Solution.java
@@ -1,5 +1,6 @@
 package quickSort;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
@@ -12,7 +13,39 @@ public class Solution {
 			arr[i]= s.nextInt();
 		}
 		s.close();
+		sort(arr,n-1);
+		System.out.println(Arrays.toString(arr));
 
+	}
+
+	private static void sort(int[] arr, int pivot) {
+		//for(int i = pivot;i<0;i++)
+		int low = 0;
+		int high = pivot-1;
+		partition(arr, low,high,pivot);
+	}
+
+	private static void partition(int[] arr, int low, int high, int pivot) {
+		
+		while(low<high){
+			while(arr[low]<arr[pivot])
+				low++;
+			while(arr[high]>arr[pivot])
+				high--;
+			swap(arr, low,high);	
+		}
+			swap(arr, low, pivot);
+		//pivot = low;
+		if(low>0){
+			partition(arr, 0, pivot-1, low-1);
+		}
+		
+	}
+	
+	private static void swap(int[] arr, int x, int y){
+		int temp = arr[x];
+		arr[x]=arr[y];
+		arr[y]=temp;
 	}
 
 }
